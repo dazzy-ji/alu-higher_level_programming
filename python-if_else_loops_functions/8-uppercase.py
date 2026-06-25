0beef2fd3f0cb84571cccd5ab7276ad03fe1f75b
dazzy-ji <d.indimuli@alustudent.com>
2026-06-25 08:00:33 -0700

    Task 8.2

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""Module that prints a string in uppercase."""
-
-
 def uppercase(str):
-    """Print str in uppercase followed by a new line."""
-    for c in str:
-        print("{}".format(
-            chr(ord(c) - 32) if ord('a') <= ord(c) <= ord('z') else c),
-            end="")
-    print("")
+    for i in str:
+        ascii_val = ord(i)
+        if 97 <= ascii_val <= 122:
+            i = chr(ascii_val - 32)
+         print("{}".format(i), end="")
+     print("")
