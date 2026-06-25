2699957d131daff71e203df4eb711b537365f8bd
dazzy-ji <d.indimuli@alustudent.com>
2026-06-25 07:57:09 -0700

    Task 8.1

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,8 +5,7 @@
 def uppercase(str):
     """Print str in uppercase followed by a new line."""
     for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            print("{}".format(c), end="")
+        print("{}".format(
+            chr(ord(c) - 32) if ord('a') <= ord(c) <= ord('z') else c),
+            end="")
     print("")
