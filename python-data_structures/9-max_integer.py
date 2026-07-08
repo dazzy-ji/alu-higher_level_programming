368b7cfed477cb4945274cc4a4e7cba7f60164a9
dazzy-ji <d.indimuli@alustudent.com>
2026-07-08 10:02:09 -0700

    Task 9.1

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,5 +1,10 @@
-#!/usr/bin/python3def max_integer(my_list=[]):
-    if len(my_list) == 0:
+#!/usr/bin/python3
+"""Module that finds the biggest integer of a list."""
+
+
+def max_integer(my_list=[]):
+    """Return the biggest integer in my_list, or None if empty."""
+    if not my_list:
         return None
     biggest = my_list[0]
     for item in my_list:
