baa0ed3458450b7094a3787e235a68ace2b97820
dazzy-ji <d.indimuli@alustudent.com>
2026-07-13 04:01:06 -0700

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+    """This module defines a class Square"""
+
+
+class Square:
+    """Represents a square"""        
+    def __init__(self, size):
+        """Initializes a square
+
+        Args:
+            size(int): The size of the square
+        """
+        self.__size = size
