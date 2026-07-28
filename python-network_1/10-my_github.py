eb1d436b8d626fd529607d208eaba2f851ffeb2e
dazzy-ji <d.indimuli@alustudent.com>
2026-07-28 08:29:10 -0700

    task 9 and 10

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     token = sys.argv[2]
     url = "https://api.github.com/user"
-    
+
     r = requests.get(url, auth=(username, token))
     print(r.json().get('id'))
--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ import requests
 if __name__ == "__main__":
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
     url = "http://0.0.0.0:5000/search_user"
-    
+
     try:
         r = requests.post(url, data={'q': letter})
         json_res = r.json()
