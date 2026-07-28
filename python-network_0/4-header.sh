fded3e96a612f2c2b861d8af14a39ae1b4399c34
dazzy-ji <d.indimuli@alustudent.com>
2026-07-28 08:16:32 -0700

    task fixed

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a GET request with a custom header X-HolbertonSchool-User-Id set to 98
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sL -H "X-HolbertonSchool-User-Id: 98" "$1"
