344d0a969c58f820c8c00b5e9f5fbf623a732cb1
dazzy-ji <d.indimuli@alustudent.com>
2026-07-28 07:47:23 -0700

    tasks fixed

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
--- Creates the user user_0d_1 with all privileges on the MySQL server
+-- Creates the MySQL user user_0d_1 with all privileges on the server
 -- The script does not fail if the user already exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
