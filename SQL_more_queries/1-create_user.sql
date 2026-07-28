ff13b9d9c1d73025b0e22c5e3ee316eec85dd48d
dazzy-ji <d.indimuli@alustudent.com>
2026-07-28 09:29:53 -0700

    changes

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- Creates the MySQL user user_0d_1 with all privileges on the server
--- The script does not fail if the user already exists
+-- Creates the MySQL server user user_0d_1 with all privileges on localhost.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+REVOKE OPTION ON *.* FROM 'user_0d_1'@'localhost';
