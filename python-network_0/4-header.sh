47b73b9d117cd353b943591150179934c9e49d41
dazzy-ji <d.indimuli@alustudent.com>
2026-07-28 08:17:55 -0700

    task 4.1

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a GET request with a custom header X-HolbertonSchool-User-Id set to 98
-curl -sL -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Script that sends a GET request with a custom header X-School-User-Id set to 98
+curl -sL -H "X-School-User-Id: 98" "$1"
